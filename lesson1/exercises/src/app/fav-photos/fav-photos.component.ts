9c35dbb2a01e1208bd4d7a672ffd3c33cf132662
Carl <carljarms@gmail.com>
2019-12-10 23:09:47 -0600

    Finished ready made components

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'My Pics';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'http://scififics.com/wp-content/uploads/2018/12/4-Witness-Header-1024x512.jpg';
+  image3 = 'http://scififics.com/wp-content/uploads/2019/07/1-Jurisdiction-Header-1024x514.jpg';
 
   constructor() { }
 
